ba8dff41a68935d596e1c76a722e536e696449b4
ScriptRUs <69376579+ScriptRUs@users.noreply.github.com>
2022-03-03 19:52:02 -0600

    Update UiLib.lua

--- UiLib.lua
+++ UiLib.lua
@@ -921,6 +921,7 @@ function Library:create(options)
 
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
 	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
+	creditsTab:credit{Name = "Ryans", Description = "Scripting Developer", Discord = "Deity#Ryans#3857"}
 
 	return mt
 end
