d0b050c684604f3af8a1a6ec528faa29674c2490
ScriptRUs <69376579+ScriptRUs@users.noreply.github.com>
2022-03-15 18:13:00 -0500

    Update UiLib.lua

--- UiLib.lua
+++ UiLib.lua
@@ -3101,7 +3101,7 @@ function Library:prompt(options)
 		BackgroundTransparency = 1,
 		AnchorPoint = Vector2.new(0, 1),
 		Position = UDim2.new(0, 0,1, -5),
-		Size = UDim2.new(1, 0,0, 20)
+		Size = UDim2.new(25, 0,0, 100)
 	})
 
 	local _gridButtonHolder = buttonHolder:object("UIGridLayout", {
