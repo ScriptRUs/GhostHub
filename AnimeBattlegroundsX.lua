2bc92390c891d2c31e6c7cc078280123b82a494e
ScriptRUs <69376579+ScriptRUs@users.noreply.github.com>
2022-03-17 18:29:47 -0500

    Delete AnimeBattlegroundsX.lua

--- AnimeBattlegroundsX.lua
+++
@@ -1,2 +0,0 @@
-game.Players.LocalPlayer:Kick("You've Joined A Discountinued Game. Game List Has Been Copied To Clipboard Along With Features & More")
-setclipboard("https://github.com/ScriptRUs/GhostHubRelated/blob/main/FeaturesList%26Invites")
